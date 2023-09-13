8ac3be82a5bfd818b4cfcbffbf5bdce4fc7bca0a
annavarner <var21033@byui.edu>
2023-09-13 19:55:23 -0400

    lesson 1 add routes

--- server.js
+++ server.js
@@ -2,8 +2,16 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
+    res.send('Mark Varner');
+});
+
+app.get('/lillian', (req, res) => {
+    res.send('Lillian Varner');
+});
+app.get('/tyler', (req, res) => {
     res.send('Tyler Varner');
 });
+
 
 const port = 3000;
 
