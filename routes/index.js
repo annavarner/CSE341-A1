1d2cbb61fedb374d350332f3ac881398f483ad6f
annavarner <var21033@byui.edu>
2023-09-14 19:52:18 -0400

    fix index.js

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,10 @@
 const routes = require('express').Router();
-
 const lesson1Controller = require('../controllers/lesson1');
 
-app.get('/', lesson1Controller.markRoute);
+routes.get('/', lesson1Controller.markRoute);
 
-app.get('/lillian', lesson1Controller.lillianRoute);
+routes.get('/lillian', lesson1Controller.lillianRoute);
 
-app.get('/tyler', lesson1Controller.tylerRoute);
+routes.get('/tyler', lesson1Controller.tylerRoute);
 
 module.exports = routes;
