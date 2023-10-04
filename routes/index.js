6efeaeb595d0ad6e490524fb35e70fb5be768ec8
annavarner <var21033@byui.edu>
2023-10-04 18:59:55 -0400

    remove index routes from swagger doc

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
-const swaggerDocument = require('../swagger-output.json');
-const swaggerUi = require('swagger-ui-express');
+// const swaggerDocument = require('../swagger-output.json');
+// const swaggerUi = require('swagger-ui-express');
 
 const baseController = require("../controllers");
 router.get("/", baseController.getName);
@@ -9,8 +9,8 @@ router.get("/", baseController.getName);
 
 router.use("/contacts", require("./contacts"));
 
-router.use('/api-docs', swaggerUi.serve);
-router.get('/api-docs', swaggerUi.setup(swaggerDocument));
+// router.use('/api-docs', swaggerUi.serve);
+// router.get('/api-docs', swaggerUi.setup(swaggerDocument));
 
 module.exports = router;
 
